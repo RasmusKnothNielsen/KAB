6847c01a5c36a5773e82c85668fb41ffada78cd4
Knud Billing <knudjbilling@gmail.com>
2019-12-02 15:06:25 +0100

    Added week number Added logo Added menu ("burger")

--- src/main/java/edu/kea/kab/controller/MainController.java
+++ src/main/java/edu/kea/kab/controller/MainController.java
@@ -1,13 +1,23 @@
 package edu.kea.kab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 @Controller
 public class MainController {
 
     @GetMapping("/input")
-    public String input() {
-        return "/input";
+    public String input(Model model) {
+        Calendar calendar = new GregorianCalendar();
+        Date trialTime = new Date();
+        calendar.setTime(trialTime);
+        System.out.println(calendar.get(Calendar.WEEK_OF_YEAR));
+        model.addAttribute("weeknumber",calendar.get(Calendar.WEEK_OF_YEAR));
+        return "input";
     }
 }
