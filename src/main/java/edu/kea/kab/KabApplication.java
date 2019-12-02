6d133a97d20b0ff24b573e7c53cdcaa0e07ab94f
Knud Billing <knudjbilling@gmail.com>
2019-12-02 10:38:25 +0100

    Initial upload for spring

---
+++ src/main/java/edu/kea/kab/KabApplication.java
@@ -0,0 +1,13 @@
+package edu.kea.kab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/kea/kab/KabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kea.kab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
