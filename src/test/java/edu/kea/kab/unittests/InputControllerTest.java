9fdd865c2bf7d92348af20ae26f8f85dcc6ab53b
Knud Billing <knudjbilling@gmail.com>
2019-12-09 15:05:06 +0100

    Added ok button

--- src/test/java/edu/kea/kab/unittests/InputControllerTest.java
+++ src/test/java/edu/kea/kab/unittests/InputControllerTest.java
@@ -3,13 +3,10 @@ package edu.kea.kab.unittests;
 import edu.kea.kab.controller.MainController;
 import edu.kea.kab.repository.ConsumptionRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
